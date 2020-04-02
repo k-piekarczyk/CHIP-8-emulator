d16909a9051432b75d867f3f4e61dd43dd3bd8f6
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2020-04-02 11:51:21 +0200

    add declarations for Timer class

--- src/Timer.h
+++ src/Timer.h
@@ -5,9 +5,21 @@
 #ifndef PROJEKT_INDYWIDUALNY_TIMER_H
 #define PROJEKT_INDYWIDUALNY_TIMER_H
 
+#include <chrono>
+
+#include "Beeper.h"
 
 class Timer {
+    unsigned char &value;
+    Beeper *beeper = nullptr;
+    std::chrono::steady_clock::time_point timer;
 
+public:
+    explicit Timer(unsigned char &v);
+
+    Timer(unsigned char &v, Beeper *beeper);
+
+    void update();
 };
 
 
