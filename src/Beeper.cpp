41c30ce38e3cd3c5154b2375ff761ac01f3397bd
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2020-04-02 11:46:27 +0200

    add Beeper class

---
+++ src/Beeper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Krzysiek on 02.04.2020.
+//
+
+#include "Beeper.h"
---
+++ src/Beeper.h
@@ -0,0 +1,14 @@
+//
+// Created by Krzysiek on 02.04.2020.
+//
+
+#ifndef PROJEKT_INDYWIDUALNY_BEEPER_H
+#define PROJEKT_INDYWIDUALNY_BEEPER_H
+
+
+class Beeper {
+
+};
+
+
+#endif //PROJEKT_INDYWIDUALNY_BEEPER_H
