3b12576d2a35675ba235c878516f5749792c85c1
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2020-04-02 11:45:23 +0200

    add Timer class

---
+++ src/Timer.h
@@ -0,0 +1,14 @@
+//
+// Created by Krzysiek on 02.04.2020.
+//
+
+#ifndef PROJEKT_INDYWIDUALNY_TIMER_H
+#define PROJEKT_INDYWIDUALNY_TIMER_H
+
+
+class Timer {
+
+};
+
+
+#endif //PROJEKT_INDYWIDUALNY_TIMER_H
