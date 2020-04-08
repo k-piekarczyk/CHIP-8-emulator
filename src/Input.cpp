366c07d9aacd1560577b9a29a38ef05a4abee925
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2020-04-08 23:02:33 +0200

    change input handling to change key status until key up, and remove redundant clearKeys() call, which made input detection in the program inconsistent

--- src/Input.cpp
+++ src/Input.cpp
@@ -10,7 +10,6 @@ Input::Input(unsigned char *keys) : keys(keys) {}
 
 void Input::update() {
     SDL_Event e;
-    clearKeys();
 
     while (SDL_PollEvent(&e)) {
         switch (e.type) {
@@ -66,6 +65,58 @@ void Input::update() {
                         break;
                     case SDLK_v:
                         keys[0xF] = 0xFF;
+                        break;
+                }
+                break;
+            case SDL_KEYUP:
+                switch (e.key.keysym.sym) {
+                    case SDLK_1:
+                        keys[0x0] = 0;
+                        break;
+                    case SDLK_2:
+                        keys[0x1] = 0;
+                        break;
+                    case SDLK_3:
+                        keys[0x2] = 0;
+                        break;
+                    case SDLK_4:
+                        keys[0x3] = 0;
+                        break;
+                    case SDLK_q:
+                        keys[0x4] = 0;
+                        break;
+                    case SDLK_w:
+                        keys[0x5] = 0;
+                        break;
+                    case SDLK_e:
+                        keys[0x6] = 0;
+                        break;
+                    case SDLK_r:
+                        keys[0x7] = 0;
+                        break;
+                    case SDLK_a:
+                        keys[0x8] = 0;
+                        break;
+                    case SDLK_s:
+                        keys[0x9] = 0;
+                        break;
+                    case SDLK_d:
+                        keys[0xA] = 0;
+                        break;
+                    case SDLK_f:
+                        keys[0xB] = 0;
+                        break;
+                    case SDLK_z:
+                        keys[0xC] = 0;
+                        break;
+                    case SDLK_x:
+                        keys[0xD] = 0;
+                        break;
+                    case SDLK_c:
+                        keys[0xE] = 0;
+                        break;
+                    case SDLK_v:
+                        keys[0xF] = 0;
                         break;
                 }
         }
