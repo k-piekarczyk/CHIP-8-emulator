c41a779909b864787d4f462fe95bf1f6fff395a8
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-04-24 00:27:29 +0200

    Prepared the repository for a switch to SFML from SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,64 @@
+//#include <iostream>
+//#include "CHIP8.h"
+//#include "Graphics.h"
+//#include "Input.h"
+//#include "Beeper.h"
+//#include "Timer.h"
+//
+//int main(int argc, char **argv) {
+//    CHIP8 chip;
+//
+//    chip.initialize();
+//
+//    Graphics g = Graphics(chip.getGFX());
+//    Input input = Input(chip.getKeys());
+//
+//    Beeper beeper = Beeper();
+//    Timer soundTimer = Timer(*chip.getSoundTimerPtr(), &beeper);
+//    Timer delayTimer = Timer(*chip.getDelayTimerPtr());
+//
+//    chip.loadRom("../roms/slipperyslope.ch8");
+//
+//    chip.loadInputHandler(&input);
+//
+//    while (!input.isFinished()) {
+//
+//        for(int i=0;i<30;i++) {
+//            soundTimer.update();
+//            delayTimer.update();
+//
+//            input.update();
+//            chip.next();
+//            g.draw();
+//        }
+//        SDL_Delay(2);
+//    }
+//
+//    return 0;
+//}
+
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
-#include "CHIP8.h"
-#include "Graphics.h"
-#include "Input.h"
-#include "Beeper.h"
-#include "Timer.h"
+int main(int argc, char ** argv){
+    sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Thingie");
 
+    sf::Event event;
 
-int main(int argc, char **argv) {
-    CHIP8 chip;
+    sf::CircleShape circleShape(200);
+    circleShape.setFillColor(sf::Color::Blue);
 
-    chip.initialize();
+    std::cout << "hello world!" << std::endl;
 
-    Graphics g = Graphics(chip.getGFX());
-    Input input = Input(chip.getKeys());
-
-    Beeper beeper = Beeper();
-    Timer soundTimer = Timer(*chip.getSoundTimerPtr(), &beeper);
-    Timer delayTimer = Timer(*chip.getDelayTimerPtr());
-
-    chip.loadRom("../roms/slipperyslope.ch8");
-
-    chip.loadInputHandler(&input);
-
-    while (!input.isFinished()) {
-
-        for(int i=0;i<30;i++) {
-            soundTimer.update();
-            delayTimer.update();
-
-            input.update();
-            chip.next();
-            g.draw();
+    while (renderWindow.isOpen()){
+        while (renderWindow.pollEvent(event)){
+            if (event.type == sf::Event::EventType::Closed)
+                renderWindow.close();
         }
-        SDL_Delay(2);
-    }
 
-    return 0;
+        renderWindow.clear();
+        renderWindow.draw(circleShape);
+        renderWindow.display();
+    }
 }
+
