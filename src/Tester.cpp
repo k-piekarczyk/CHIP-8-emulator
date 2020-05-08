b5d831a1f5fe60c33aa799f2216527c10ba04e55
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-05-08 14:51:08 +0200

    Add scaffold for Tester class

--- src/Tester.cpp
+++ src/Tester.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "Tester.hpp"
+
+void Tester::runTests() {
+    TestOutcome testOutcomes[35];
+
+    for (int i = 0; i < 35; i++) {
+        beforeEach();
+        testOutcomes[i] = opcode_test_arr[i]();
+    }
+}
+
+void Tester::initialize() {
+    opcode_test_arr[0] = &Tester::op_00E0_test_;
+    opcode_test_arr[1] = &Tester::op_00EE_test_;
+    opcode_test_arr[2] = &Tester::op_0nnn_test_;
+    opcode_test_arr[3] = &Tester::op_1nnn_test_;
+    opcode_test_arr[4] = &Tester::op_2nnn_test_;
+    opcode_test_arr[5] = &Tester::op_3xkk_test_;
+    opcode_test_arr[6] = &Tester::op_4xkk_test_;
+    opcode_test_arr[7] = &Tester::op_5xy0_test_;
+    opcode_test_arr[8] = &Tester::op_6xkk_test_;
+    opcode_test_arr[9] = &Tester::op_7xkk_test_;
+    opcode_test_arr[10] = &Tester::op_8xy0_test_;
+    opcode_test_arr[11] = &Tester::op_8xy1_test_;
+    opcode_test_arr[12] = &Tester::op_8xy2_test_;
+    opcode_test_arr[13] = &Tester::op_8xy3_test_;
+    opcode_test_arr[14] = &Tester::op_8xy4_test_;
+    opcode_test_arr[15] = &Tester::op_8xy5_test_;
+    opcode_test_arr[16] = &Tester::op_8xy6_test_;
+    opcode_test_arr[17] = &Tester::op_8xy7_test_;
+    opcode_test_arr[18] = &Tester::op_8xyE_test_;
+    opcode_test_arr[19] = &Tester::op_9xy0_test_;
+    opcode_test_arr[20] = &Tester::op_Annn_test_;
+    opcode_test_arr[21] = &Tester::op_Bnnn_test_;
+    opcode_test_arr[22] = &Tester::op_Cxkk_test_;
+    opcode_test_arr[23] = &Tester::op_Dxyn_test_;
+    opcode_test_arr[24] = &Tester::op_Ex9E_test_;
+    opcode_test_arr[25] = &Tester::op_ExA1_test_;
+    opcode_test_arr[26] = &Tester::op_Fx07_test_;
+    opcode_test_arr[27] = &Tester::op_Fx0A_test_;
+    opcode_test_arr[28] = &Tester::op_Fx15_test_;
+    opcode_test_arr[29] = &Tester::op_Fx18_test_;
+    opcode_test_arr[30] = &Tester::op_Fx1E_test_;
+    opcode_test_arr[31] = &Tester::op_Fx29_test_;
+    opcode_test_arr[32] = &Tester::op_Fx33_test_;
+    opcode_test_arr[33] = &Tester::op_Fx55_test_;
+    opcode_test_arr[34] = &Tester::op_Fx65_test_;
+}
--- src/Tester.hpp
+++ src/Tester.hpp
@@ -6,8 +6,97 @@
 #define PROJEKT_INDYWIDUALNY_TESTER_H
 
 
-class Tester {
+#include "CHIP8.hpp"
 
+class Tester {
+    CHIP8 * chip = nullptr;
+
+    TestOutcome (Tester::*opcode_test_arr[35])();
+
+    struct TestOutcome {
+        char * name;
+        bool success;
+        char * message;
+    };
+
+public:
+    void initialize();
+    void runTests();
+
+private:
+    void beforeEach();
+
+    void runCurrentOpcode();
+
+    TestOutcome op_00E0_test_();
+
+    TestOutcome op_00EE_test_();
+
+    TestOutcome op_0nnn_test_();
+
+    TestOutcome op_1nnn_test_();
+
+    TestOutcome op_2nnn_test_();
+
+    TestOutcome op_3xkk_test_();
+
+    TestOutcome op_4xkk_test_();
+
+    TestOutcome op_5xy0_test_();
+
+    TestOutcome op_6xkk_test_();
+
+    TestOutcome op_7xkk_test_();
+
+    TestOutcome op_8xy0_test_();
+
+    TestOutcome op_8xy1_test_();
+
+    TestOutcome op_8xy2_test_();
+
+    TestOutcome op_8xy3_test_();
+
+    TestOutcome op_8xy4_test_();
+
+    TestOutcome op_8xy5_test_();
+
+    TestOutcome op_8xy6_test_();
+
+    TestOutcome op_8xy7_test_();
+
+    TestOutcome op_8xyE_test_();
+
+    TestOutcome op_9xy0_test_();
+
+    TestOutcome op_Annn_test_();
+
+    TestOutcome op_Bnnn_test_();
+
+    TestOutcome op_Cxkk_test_();
+
+    TestOutcome op_Dxyn_test_();
+
+    TestOutcome op_Ex9E_test_();
+
+    TestOutcome op_ExA1_test_();
+
+    TestOutcome op_Fx07_test_();
+
+    TestOutcome op_Fx0A_test_();
+
+    TestOutcome op_Fx15_test_();
+
+    TestOutcome op_Fx18_test_();
+
+    TestOutcome op_Fx1E_test_();
+
+    TestOutcome op_Fx29_test_();
+
+    TestOutcome op_Fx33_test_();
+
+    TestOutcome op_Fx55_test_();
+
+    TestOutcome op_Fx65_test_();
 };
 
 
