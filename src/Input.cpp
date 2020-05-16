ab24108bd4768fc852d2e7e35e7ffe62d2e313f3
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-05-16 23:50:59 +0200

    make key handling more readable

--- src/Input.cpp
+++ src/Input.cpp
@@ -4,7 +4,7 @@
 
 #include "Input.hpp"
 
-Input::Input(unsigned char * keys, sf::RenderWindow *window) : keys(keys), window(window) {}
+Input::Input(unsigned char *keys, sf::RenderWindow *window) : keys(keys), window(window) {}
 
 void Input::update() {
     sf::Event event{};
@@ -30,128 +30,28 @@ void Input::update() {
 void Input::await() {
     sf::Event event{};
     do window->waitEvent(event);
-    while(!(event.type == sf::Event::KeyPressed && handleKeyPressedEvents(event)));
+    while (!(event.type == sf::Event::KeyPressed && handleKeyPressedEvents(event)));
 }
 
 bool Input::handleKeyPressedEvents(sf::Event &event) {
-    bool legalKey = true;
+    if (event.key.code == sf::Keyboard::Escape) window->close();
+    else
+        for (int i = 0; i < 0x10; i++) {
+            if (event.key.code == keyMap[i]) {
+                keys[i] = 0xFF;
+                return true;
+            }
+        }
 
-    switch (event.key.code) {
-        case sf::Keyboard::Escape:
-            window->close();
-            break;
-        case sf::Keyboard::Num1:
-            keys[0x1] = 0xFF;
-            break;
-        case sf::Keyboard::Num2:
-            keys[0x2] = 0xFF;
-            break;
-        case sf::Keyboard::Num3:
-            keys[0x3] = 0xFF;
-            break;
-        case sf::Keyboard::Num4:
-            keys[0xC] = 0xFF;
-            break;
-        case sf::Keyboard::Q:
-            keys[0x4] = 0xFF;
-            break;
-        case sf::Keyboard::W:
-            keys[0x5] = 0xFF;
-            break;
-        case sf::Keyboard::E:
-            keys[0x6] = 0xFF;
-            break;
-        case sf::Keyboard::R:
-            keys[0xD] = 0xFF;
-            break;
-        case sf::Keyboard::A:
-            keys[0x7] = 0xFF;
-            break;
-        case sf::Keyboard::S:
-            keys[0x8] = 0xFF;
-            break;
-        case sf::Keyboard::D:
-            keys[0x9] = 0xFF;
-            break;
-        case sf::Keyboard::F:
-            keys[0xE] = 0xFF;
-            break;
-        case sf::Keyboard::Z:
-            keys[0xA] = 0xFF;
-            break;
-        case sf::Keyboard::X:
-            keys[0x0] = 0xFF;
-            break;
-        case sf::Keyboard::C:
-            keys[0xB] = 0xFF;
-            break;
-        case sf::Keyboard::V:
-            keys[0xF] = 0xFF;
-            break;
-        default:
-            legalKey = false;
-            break;
-    }
-
-    return legalKey;
+    return false;
 }
 
 bool Input::handleKeyReleasedEvents(sf::Event &event) {
-    bool legalKey = true;
+    for (int i = 0; i < 0x10; i++)
+        if (event.key.code == keyMap[i]) {
+            keys[i] = 0;
+            return true;
+        }
 
-    switch (event.key.code) {
-        case sf::Keyboard::Num1:
-            keys[0x1] = 0;
-            break;
-        case sf::Keyboard::Num2:
-            keys[0x2] = 0;
-            break;
-        case sf::Keyboard::Num3:
-            keys[0x3] = 0;
-            break;
-        case sf::Keyboard::Num4:
-            keys[0xC] = 0;
-            break;
-        case sf::Keyboard::Q:
-            keys[0x4] = 0;
-            break;
-        case sf::Keyboard::W:
-            keys[0x5] = 0;
-            break;
-        case sf::Keyboard::E:
-            keys[0x6] = 0;
-            break;
-        case sf::Keyboard::R:
-            keys[0xD] = 0;
-            break;
-        case sf::Keyboard::A:
-            keys[0x7] = 0;
-            break;
-        case sf::Keyboard::S:
-            keys[0x8] = 0;
-            break;
-        case sf::Keyboard::D:
-            keys[0x9] = 0;
-            break;
-        case sf::Keyboard::F:
-            keys[0xE] = 0;
-            break;
-        case sf::Keyboard::Z:
-            keys[0xA] = 0;
-            break;
-        case sf::Keyboard::X:
-            keys[0x0] = 0;
-            break;
-        case sf::Keyboard::C:
-            keys[0xB] = 0;
-            break;
-        case sf::Keyboard::V:
-            keys[0xF] = 0;
-            break;
-        default:
-            legalKey = false;
-            break;
-    }
-
-    return legalKey;
+    return false;
 }
--- src/Input.hpp
+++ src/Input.hpp
@@ -12,6 +12,24 @@
 class Input {
     unsigned char * keys;
     sf::RenderWindow *window;
+    sf::Keyboard::Key keyMap[0x10] = {
+        sf::Keyboard::X,
+        sf::Keyboard::Num1,
+        sf::Keyboard::Num2,
+        sf::Keyboard::Num3,
+        sf::Keyboard::Q,
+        sf::Keyboard::W,
+        sf::Keyboard::E,
+        sf::Keyboard::A,
+        sf::Keyboard::S,
+        sf::Keyboard::D,
+        sf::Keyboard::Z,
+        sf::Keyboard::C,
+        sf::Keyboard::Num4,
+        sf::Keyboard::R,
+        sf::Keyboard::F,
+        sf::Keyboard::V
+    };
 
 public:
     explicit Input(unsigned char * keys, sf::RenderWindow *window);
